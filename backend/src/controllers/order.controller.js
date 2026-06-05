d10e61bb964bc0dc68ef2ac848e34fd78b6ef35d
sanathecoder <sanasana33377737@gmail.com>
2026-06-05 21:50:40 +0500

    update

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,6 +1,55 @@
 const orderModel = require('../models/order.model')
+const cartModel = require('../models/cart.model')
 
 async function placeOrder(req,res){
-    const {shippingAddress,phone} = req.body
-    const 
+ try {
+    
+       const {shippingAddress,phone} = req.body
+
+    const cart = await cartModel.findOne({
+        user: req.user._id
+    }).populate("products.product")
+
+    if(!cart || cart.products.length === 0){
+        return res.status(404).json({
+            message: "cart is empty"
+        })
+    }
+
+    let totalAmount = 0;
+    const orderProducts = cart.products.map((item)=>{
+        totalAmount += item.product.price * item.quantity
+
+        return{
+            product: item.product._id,
+            quantity: item.quantity,
+            price: item.product.price
+        }
+        })
+
+        const order = await OrderModel.create({
+      user: req.user._id,
+      products: orderProducts,
+      shippingAddress,
+      phone,
+      totalAmount,
+    });
+
+    cart.products= []
+    await cart.save()
+
+     res.status(201).json({
+      message: "Order placed successfully",
+      order,
+    });
+
+ } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+    
+ }
+
 }
+
+module.exports ={placeOrder}
