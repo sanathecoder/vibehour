9b9dd509e51bb7088a3a1fa3855c50f2832536db
sanathecoder <sanasana33377737@gmail.com>
2026-07-06 23:36:55 +0500

    chnages

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api" || "vibehour-production.up.railway.app/api",
+  baseURL: "https://vibehour-production.up.railway.app",
   withCredentials: true,
 });
 
