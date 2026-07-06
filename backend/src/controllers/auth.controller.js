5293bc5716317d5e58c0747d0aa89df3cd7da11d
sanathecoder <sanasana33377737@gmail.com>
2026-07-07 00:36:23 +0500

    some changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -74,7 +74,8 @@ async function Login(req, res) {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production", // Production me automatic true ho jayega
-            sameSite: "strict",
+            secure: true, 
+    sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
