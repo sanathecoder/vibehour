f07f5ab168ef31d0e2a43b54e27e2e0bc0bdcbc9
sanathecoder <sanasana33377737@gmail.com>
2026-07-06 23:28:50 +0500

    change again

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173" || "https://vibehour.vercel.app/",
+    origin:  ["https://vibehour.vercel.app/" , "http://localhost:5173"],
     credentials: true,
 }))
 app.use(cookieParser())
